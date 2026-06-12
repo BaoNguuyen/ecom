cbe2c39d6060158eeafbb1fee0dfd5df1de250db
Nguyen Van Bao <104445777+BaoNguuyen@users.noreply.github.com>
2026-06-12 10:25:49 +0700

    #issue - 0003: Provider Modal

--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -3,17 +3,27 @@
 import { createContext, useContext, useState } from "react";
 
 type ModalContextType = {
+    isOpen: boolean;
+    openModal: () => void;
+    closeModal: () => void;
 };
 
 
 const ModalContext = createContext<ModalContextType | null>(null);
 
-export function ModalProvider() {
+export function ModalProvider({children}: {children: React.ReactNode}) {
     const [isOpen, setIsOpen] = useState(false)
 
+    const value = {
+        isOpen,
+        openModal: () => setIsOpen(true),
+        closeModal: () => setIsOpen(false),
+      };
+
     return (
-        <>
-        </>
+        <ModalContext.provider value={value}>
+            {children}
+        <ModalContext.provider/>
     )
 }
 
